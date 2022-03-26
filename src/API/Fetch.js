4b574fffdf17ff68eac96251a64fbcd459c61adf
MShMahdiye <mshcomen8013@gmail.com>
2022-03-26 22:44:03 +0430

    First commit

---
+++ src/API/Fetch.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import axios from 'axios';
+
+const URL = `https://api.openweathermap.org/data/2.5/weather`;
+const API_KEY = 'f33a484cf794d08d0148764789aaba32';
+
+const Fetch = async (city) => {
+
+  let queryParameters = `q=${city}&units='metric'&APPID=${API_KEY}`
+
+  const data = await fetch(`${URL}?${queryParameters}`).then((res) => {return res.json();})
+  return data;
+
+}
+
+export default Fetch
--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import Fetch from './API/Fetch';
+import { useState } from 'react';
 
 function App() {
+
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState({})
+
+  const getValue = (e) => {
+    setCity(e.target.value);
+  }
+
+  const handleKeyPress = async (e) => {
+    if (e.key === 'Enter') {
+      const data = await Fetch(city)
+      setWeather(data)
+      setCity('')
+      console.log('Data from handleKeyPress in App.js : ', data);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <input
+        type={'text'}
+        value={city}
+        onChange={getValue}
+        placeholder='Search here'
+        className='search-inp'
+        onKeyPress={handleKeyPress}
+      />
+      {
+        weather.main && (
+          <div className='weather-container-city'>
+            <h2 className='city-name'>
+              <span>
+                {weather.name}
+              </span>
+              <sup>
+                {weather.sys.country}
+              </sup>
+            </h2>
+            <div>
+              {Math.round(weather.main.temp)}<sup>&deg;C</sup>
+            </div>
+            <div className="info">
+              <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+              <p>{weather.weather[0].description}</p>
+            </div>
+          </div>
+        )
+      }
     </div>
   );
 }
