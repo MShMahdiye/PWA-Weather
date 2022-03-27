ce7066c8abf580427de20cab9ff29fc46f409833
MShMahdiye <mshcomen8013@gmail.com>
2022-03-27 19:27:46 +0430

    serviceWorker created

---
+++ public/serviceworker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'version-1';
+const urlsToCache = ['index.html','offline.html'];
+
+const self = this;
+
+self.addEventListener('install',(event) =>{
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) =>{
+    console.log('Cache is opened');
+    return cache.addAll(urlsToCache);
+  }))
+})
+
+self.addEventListener('fetch',(event) => {
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(
+        () => {
+          caches.match('offline.html')
+        }
+      )
+    })
+  )
+})
+
+self.addEventListener('activate',
+(event) => {
+  const targetCacheList = [];
+
+  targetCacheList.push(CACHE_NAME);
+
+  event.waitUntil(
+    caches.keys().then(
+      (cacheNames) => Promise.all(cacheNames.map(
+          (cacheName) => {
+            if(!targetCacheList.includes(cacheName)){
+              return caches.delete(cacheName);
+            }
+          }
+        ))
+    )
+  )
+})
