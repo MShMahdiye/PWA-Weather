3a8504c86ec2df312d1859b41acf11b57bea3fd1
MShMahdiye <mshcomen8013@gmail.com>
2022-03-28 12:49:20 +0430

    Title changed

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -13,11 +13,8 @@ self.addEventListener('install',(event) =>{
 self.addEventListener('fetch',(event) => {
   event.respondWith(
     caches.match(event.request).then(() => {
-      return fetch(event.request).catch(
-        () => {
-          caches.match('offline.html')
-        }
-      )
+      console.log('from fetch and match !');
+      return fetch(event.request).catch(() => caches.match('offline.html'))
     })
   )
 })
